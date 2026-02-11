88d74f8aa7671c1934d9ee98bf08d0aa3a6bbaf8
Ritesh22497 <riteshthakur22497@gmail.com>
2026-02-11 11:56:05 +0530

    done

--- lib/bloc/login_bloc.dart
+++ lib/bloc/login_bloc.dart
@@ -1,8 +1,25 @@
+import 'dart:convert';
 
-    import 'package:http/http.dart' as http;
-    
-    class LoginBloc {
-      Future<void> login() async {
-        await http.post(Uri.parse('https://example.com/login'));
-      }
-    }
+import 'package:http/http.dart'as http;
+
+class ApiCall{
+
+Future <dynamic>  getmtted() async {
+try{
+  var url="http/logi";
+      Map<String, String>? header={};
+      Object? body={"text":"tect",};
+   var responce= await  http.post(Uri.parse(url,),headers:header,body: jsonEncode(body)  );
+   if(responce.statusCode==200){
+    var res=responce.body; 
+    return res;                          
+   }
+}catch(e){
+
+}
+
+
+}
+
+}
+      
