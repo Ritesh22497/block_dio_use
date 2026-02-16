95317c9d8fb000087314770b92b2706a5b3cf56b
Ritesh22497 <riteshthakur22497@gmail.com>
2026-02-16 14:36:13 +0530

    done

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -9,9 +9,9 @@ plugins {
 }
 
 android {
-    namespace = "com.example.block_dio_use"
+    namespace = "com.example.login"
     compileSdk = 35
-    ndkVersion  ="27.0.12077973"
+    ndkVersion = "27.0.12077973"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -24,11 +24,11 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.block_dio_use"
+        applicationId = "com.example.login"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
-        targetSdk = 25
+        minSdk = 24
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+      theme: ThemeData(                     
        
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       ),
